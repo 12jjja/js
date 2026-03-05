828438555102f946282d4030f96136c691cc048d
ming <ming.chen@coolkit.cn>
2026-03-05 11:17:33 +0800

    变量

---
+++ 2 chapter/variable.js
@@ -0,0 +1,24 @@
+// 定义单个变量
+let message;
+message = 'hello world';
+alert('hello world');
+
+// 还可以定义多个变量，有几种不同的方式
+let person;
+let person2;
+
+let person3, person4;
+
+let person5,
+person6,
+person7;
+
+let person8
+,person9
+,person10;
+
+// 除了let，旧版本中还可以使用var来声明一个变量，大差不差，具体的细节后续章节说明。
+
+// 以下方式声明变量会报错：
+let person11;
+let person11;
