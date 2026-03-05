3b179bce43747709936630dde8270c09f8507f73
ming <ming.chen@coolkit.cn>
2026-03-05 11:51:01 +0800

    变量

---
+++ 2 chapter/chapter_test.js
@@ -0,0 +1,9 @@
+// 任务1
+let admin, name;
+name = 'John';
+admin = name;
+alert(admin);
+
+//任务2
+let myPlanet = '地球';
+let currentViewer = 'Ming';
---
+++ 2 chapter/constants.js
@@ -0,0 +1,6 @@
+const myBirthday = '2003-06-10';
+myBirthday = '2003-06-12';
+alert(myBirthday);
+
+const COLOR_ORANGE = '#FF7F00';
+let backgroundColor = COLOR_ORANGE;
